8806aa9314170d8ed48aad945de36a5a09ce10d6
Bhavishyapuchakayala <75924671+Bhavishyapuchakayala@users.noreply.github.com>
2021-10-01 16:06:11 +0530

    Update 1373A.py

--- 1373A.py
+++ 1373A.py
@@ -1,6 +1,11 @@
-for _ in range(int(input())):
-    a,b,c=map(int,input().split())
-    d,e=-1,-1
-    if a<c:d=1
-    if a*b>c:e=b
-    print(d,e)
+def buying_donuts():
+    for _ in range(int(input())):
+        a,b,c=map(int,input().split())
+        d,e=-1,-1
+        if a<c:d=1
+        if a*b>c:e=b
+        print(d,e)
+        
+if __name__ == "__main__":
+    buying_donuts()
+        
