ce83f3208259ee06d88158295de0f2b349df028a
saikarthik2 <91731961+saikarthik2@users.noreply.github.com>
2021-10-01 16:38:53 +0530

    modified the code in a better way

--- 1388B.py
+++ 1388B.py
@@ -1,4 +1,8 @@
-for _ in range(int(input())):
-    n=int(input())
-    x=(n+3)//4
-    print("9"*(n-x)+"8"*(x))
+# modified the code in a better way
+def solve:
+    for _ in range(int(input())):
+        m1=int(input())
+        x=(m1+3)//4
+        print("9"*(m1-x)+"8"*(x))
+ 
+solve();
