0ac6986a544bc195074e7e10dce211da3088d982
saikarthik2 <91731961+saikarthik2@users.noreply.github.com>
2021-10-01 16:34:23 +0530

    reduced time and space complexity

--- 1384A.py
+++ 1384A.py
@@ -1,17 +1,21 @@
-for _ in range(int(input())):
-    n=int(input())
-    a=list(map(int,input().split()))
-    b=["a"*50];val=98
-    for i in range(n):
-        s=(b[i][0:a[i]])
-        if chr(val)==b[i][-1]:
+# reduced the time and space complexity
+def solve:
+    for _ in range(int(input())):
+        m=int(input())
+        a=list(map(int,input().split()))
+        b=["a"*50];val=98
+        for i in range(m):
+            s=(b[i][0:a[i]])
+            if chr(val)==b[i][-1]:
+                val+=1
+                if val==123:
+                    val=97
+            s+=chr(val)*(50-a[i])
+            b+=[s]
             val+=1
             if val==123:
                 val=97
-        s+=chr(val)*(50-a[i])
-        b+=[s]
-        val+=1
-        if val==123:
-            val=97
-    for i in range(n+1):
-        print(b[i])
+        for i in range(m+1):
+            print(b[i])
+        
+solve();
