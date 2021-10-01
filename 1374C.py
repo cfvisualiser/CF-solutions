c712d2f6f3d8a6ffb4b3c909388888da16359027
Sai Jeevan Puchakayala <saijeevan2002@gmail.com>
2021-10-01 15:53:50 +0530

    Update 1374C.py

--- 1374C.py
+++ 1374C.py
@@ -1,12 +1,18 @@
-for _ in range(int(input())):
-    n=int(input())
-    s=input();c=0;ans=0
-    for i in range(n):
-        if s[i]=="(":
-            c+=1
-        else:
-            c-=1
-        if c<0:
-            ans+=1
-            c=0
-    print(ans)
+def minimum_moves():
+
+    for _ in range(int(input())):
+        n=int(input())
+        s=input();c=0;ans=0
+        for i in range(n):
+            if s[i]=="(":
+                c+=1
+            else:
+                c-=1
+            if c<0:
+                ans+=1
+                c=0
+        print(ans)
+
+
+if __name__ == "__main__":
+    minimum_moves()
