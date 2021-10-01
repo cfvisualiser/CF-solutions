ec85b93b81561e886b92267ca3c5d443be20dca2
Sai Jeevan Puchakayala <saijeevan2002@gmail.com>
2021-10-01 15:51:02 +0530

    Update 1371C.py

--- 1371C.py
+++ 1371C.py
@@ -1,9 +1,13 @@
-for _ in range(int(input())):
-    a,b,c,d=map(int,input().split());mi=min(a,b);ma=max(a,b)
-    if a+b<c+d:
-        print("No")
-    elif mi>=d and ma+(mi-d)>=c:
-        print("Yes")
-    else:
-        print("No")
+def no_guests_get_angry():
+    for _ in range(int(input())):
+        a,b,c,d=map(int,input().split());mi=min(a,b);ma=max(a,b)
+        if a+b<c+d:
+            print("No")
+        elif mi>=d and ma+(mi-d)>=c:
+            print("Yes")
+        else:
+            print("No")
+
+if __name__ == "__main__":
+    no_guests_get_angry()
     
