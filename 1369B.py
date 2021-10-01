72217f24dbf029afb694ed4bbd47264c0835f56a
Bhavishyapuchakayala <75924671+Bhavishyapuchakayala@users.noreply.github.com>
2021-10-01 16:02:36 +0530

    Update 1369B.py

--- 1369B.py
+++ 1369B.py
@@ -1,14 +1,18 @@
-for _ in range(int(input())):
-    n=int(input())
-    s=input();f=-1;k=-1
-    for i in range(n):
-        if s[i]=="1":
-            f=i
-            break
-    for j in range(n):
-        if s[j]=="0":
-            k=j
-    if f>=k:
-        print(s)
-    else:
-        print(s[:f]+s[k:])
+def cleanest_possible_string():
+    for _ in range(int(input())):
+        n=int(input())
+        s=input();f=-1;k=-1
+        for i in range(n):
+            if s[i]=="1":
+                f=i
+                break
+        for j in range(n):
+            if s[j]=="0":
+                k=j
+        if f>=k:
+            print(s)
+        else:
+            print(s[:f]+s[k:])
+            
+if __name__ == "__main__":
+    cleanest_possible_string()
