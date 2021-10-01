86b18ac31f447994f45e0348a3d524290b3396c7
Bhavishyapuchakayala <75924671+Bhavishyapuchakayala@users.noreply.github.com>
2021-10-01 16:09:18 +0530

    Update 1371B.py

--- 1371B.py
+++ 1371B.py
@@ -1,6 +1,11 @@
+def Only_shapes():
 for _ in range(int(input())):
     n,r=map(int,input().split())
     if n>r:
         print((r*(r+1))//2)
     else:
         print(((n*(n-1))//2)+1)
+
+
+if __name__ == "__main__":
+    Only_shapes()
