ed9d9ad2f5d53cca1497c6b88c12da795fb82fbe
Bhavishyapuchakayala <75924671+Bhavishyapuchakayala@users.noreply.github.com>
2021-10-01 16:13:54 +0530

    Update 1373B.py

--- 1373B.py
+++ 1373B.py
@@ -1,9 +1,20 @@
-for _ in range(int(input())):
-    s=input()
-    zero,one=0,0
-    for i in range(len(s)):
-        if s[i]=="0":zero+=1
-        else:one+=1
-    ans=min(zero,one)
-    if ans%2:print("DA")
-    else:print("NET")
+def moves_possible():
+    for _ in range(int(input())):
+        s=input()
+        zero,one=0,0
+        for i in range(len(s)):
+            if s[i]=="0":
+                zero+=1
+            else:
+                one+=1
+        ans=min(zero,one)
+        if ans%2:
+            print("DA")
+        else:
+            print("NET")
+if __name__ == "__main__":
+    moves_possible()
+            
+      
+            
+   
