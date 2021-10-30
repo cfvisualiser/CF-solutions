ee7d590469a5e99b52f77fee01304a0fa3d7333e
KrishnaSai_V <56120062+kittu2539@users.noreply.github.com>
2021-10-30 15:36:21 +0530

    update 1317C.py
    optimised and added comments to the code

--- 1373C.py
+++ 1373C.py
@@ -1,11 +1,18 @@
 def get_res_value():
     for _ in range(int(input())):
         s=input()
-        ans=len(s);cur=0
-        for i in range(len(s)):
-            if s[i]=="-":cur-=1
-            else:cur+=1
-            if cur<0:ans+=i+1;cur=0
+        ans=len(s)
+        cur=0
+        n=len(s)
+        #forloop
+        for i in range(0,n):
+            if s[i]=="-":
+                cur=cur-1 #update cur
+            else:
+                cur=cur+1 #update cur
+            if cur<0:
+                ans=ans+i+1 #update ans
+                cur=0
         print(ans)
 
 
