ee8dbd24cad885b4510b609385f025af1ea33800
saikarthik2602 <56120462+saikarthik2602@users.noreply.github.com>
2022-10-05 22:36:15 +0530

    Update 1388B.py
    Added a new method

--- 1388B.py
+++ 1388B.py
@@ -4,5 +4,11 @@ def solve:
         m1=int(input())
         x=(m1+3)//4
         print("9"*(m1-x)+"8"*(x))
- 
+    p=100
+    q=1
+    while(p<0):
+        p=p-1
+        q=q*2
+    q=sqrt(q+1)
+    q=log(q)
 solve();
